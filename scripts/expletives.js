93059ae63f54e35888c50e96cd80f544625afe5d
Chris McDonald <chrislyonsf@gmail.com>
2021-01-10 10:52:07 -0500

    Made expletive.js more readeable

--- scripts/expletives.js
+++ scripts/expletives.js
@@ -24,6 +24,7 @@ const strictExpletives = {
 	hoe: "phloshdradomous",
 	chink: "phlomingoromous",
 	fk: "phlwflff",
+	시발: "힝나삐짐",
 };
 
 // Expletive Filter
@@ -35,26 +36,62 @@ function explicitWordFilter(message) {
 		console.log(`Edited ${message.content} to ${global.newMessage}`);
 		message.reply("\nHey! \nI said no bad words! If you don't want to play nice, I'm taking my ball and going home!");
 	}
-	function check() {
-		global.contentArray = message.content.split(/([^\w])/);
-		foundExpletive = false;
-		global.contentArray.forEach((word, index) => {
-			Object.keys(expletives).forEach((key, v) => {
-				if (word.includes(key)) {
-					foundExpletive = true;
-					global.contentArray[index] = expletives[key];
-				}
-			});
-			Object.keys(strictExpletives).forEach((key, v) => {
-				if (word === key) {
-					foundExpletive = true;
-					global.contentArray[index] = strictExpletives[key];
-				}
-			});
-		});
-		global.newMessage = global.contentArray.join("");
-		return foundExpletive;
-	}
 }
+
+/* Checks if message has expletives */
+function check() {
+	global.contentArray = message.content.split(/([^\w])/);
+	// Filters out null indexes of array.
+	noNulls = global.contentArray.filter(Boolean);
+
+	console.log(`CONTENT ARRAY: ${noNulls}`);
+	foundExpletive = false;
+	noNulls.forEach((word, index) => {
+		/* Filters out any string that includes expletive */
+		Object.keys(expletives).forEach((key, v) => {
+			if (word.includes(key)) {
+				foundExpletive = true;
+				noNulls[index] = expletives[key];
+			}
+		});
+		/* Only filters out exact matches for strictExpletives */
+		Object.keys(strictExpletives).forEach((key, v) => {
+			if (word === key) {
+				foundExpletive = true;
+				noNulls[index] = strictExpletives[key];
+			}
+		});
+	});
+	global.newMessage = noNulls.join("");
+	return foundExpletive;
+}
+
+/* ----------------- TO DO ----------------- */
+/* Make Korean Filter */
+// function checkKorean() {
+// 	global.contentArray = message.content.split(/([^\w])/);
+// 	// Filters out null indexes of array.
+// 	noNulls = global.contentArray.filter(Boolean);
+
+// 	console.log(`CONTENT ARRAY: ${noNulls}`);
+// 	foundExpletive = false;
+// 	noNulls.forEach((word, index) => {
+// 		Object.keys(expletives).forEach((key, v) => {
+// 			if (word.includes(key)) {
+// 				foundExpletive = true;
+// 				noNulls[index] = expletives[key];
+// 			}
+// 		});
+// 		Object.keys(strictExpletives).forEach((key, v) => {
+// 			if (word === key) {
+// 				foundExpletive = true;
+// 				noNulls[index] = strictExpletives[key];
+// 			}
+// 		});
+// 	});
+// 	global.newMessage = noNulls.join("");
+// 	return foundExpletive;
+// }
+/* ------------------------------------------------ */
 
 module.exports = { explicitWordFilter };
