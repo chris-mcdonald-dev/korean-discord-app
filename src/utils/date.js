bd79f37cd0e5b6588c405159bbe219847d0b478e
Chris McDonald <chris.mcdonald93@gmail.com>
2021-02-02 02:07:36 -0500

    Add clarification to registered session msg

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,6 +1,21 @@
-function getUTCFullDate(date) {
+function getUTCFullDate(date, separateIntoGroups = "") {
 	if (!date) return "";
 	const dateUTC = date.toUTCString();
+
+	// Optionally separate date, year, time, and timezone into groups
+	const separateRegEx = /(?<onlyDate>\w{3},\s\d+\s\w{3})\s(?<year>\d{4})\s(?<time>\d+:\d+).+(?<zone>\w{3})/;
+	const { onlyDate, year, time, zone } = dateUTC.match(separateRegEx).groups;
+	switch (separateIntoGroups) {
+		case "date":
+			return onlyDate;
+		case "year":
+			return year;
+		case "time":
+			return time;
+		case "zone":
+			return zone;
+	}
+
 	return dateUTC.substr(0, dateUTC.length - 7); // Remove seconds and GMT string
 }
 
